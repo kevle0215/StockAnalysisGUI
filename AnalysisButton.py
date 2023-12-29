8c13ffdd0c95d53494c9e473bbfb2faf293d3fa3
“Kevin <kevin.lee@duke.edu>
2023-12-28 23:47:40 -0500

    added gitignore

--- AnalysisButton.py
+++ AnalysisButton.py
@@ -1,16 +1,5 @@
 import tkinter as tk
 import SupportResistance
-import StockChartWindow
-from tkinter import ttk
-import yfinance as yf
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from mplfinance.original_flavor import candlestick_ohlc
-from matplotlib.dates import DateFormatter, date2num
-import datetime
-import matplotlib.dates as mdates
 
 
 def analyze(symbol, start_date, end_date, interval, touches, sensitivity, text_box):
